140ff1dced03fd89fe77ee456ab3bf8a52572116
mrvauxs <mrvauxs@gmail.com>
2025-08-26 16:40:13 +0200

    ffs

---
+++ scripts/game-mod.js
@@ -0,0 +1,8 @@
+// Adds new Weapon types
+Hooks.once("init", () => {
+  CONFIG.DND5E.weaponIds.blunderbuss = "Compendium.the-gunner-lt.the-gunner.Item.wOJERi9qhxXoUZhi";
+  CONFIG.DND5E.weaponIds.musket = "Compendium.the-gunner-lt.the-gunner.Item.AA972pq8Ej88TUrw";
+  CONFIG.DND5E.weaponIds.pistol = "Compendium.the-gunner-lt.the-gunner.Item.1Iqv9Eh1uaZKDOGW";
+  CONFIG.DND5E.weaponIds.revolver = "Compendium.the-gunner-lt.the-gunner.Item.6epOGGYICZXg0mIZ";
+  CONFIG.DND5E.weaponIds.rifle = "Compendium.the-gunner-lt.the-gunner.Item.jqDhznM30LUiOFc1";
+});
